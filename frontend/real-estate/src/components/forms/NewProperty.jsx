15123e9bbb8cab9e5f4c02560edec96df1630d77
abrokinla <abrokinla@gmail.com>
2023-01-22 23:56:14 +0100

    Add new property

--- frontend/real-estate/src/components/forms/NewProperty.jsx
+++ frontend/real-estate/src/components/forms/NewProperty.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import NavBar from "../NavBar";
 import "../../styles/newproperty.css"
 
 const NewProperty = () => {
@@ -38,102 +39,121 @@ const NewProperty = () => {
       
 
     return (
-        <>
-            <form>
-                <div className="draggable-item" draggable={true} onDragStart={handleDragStart} onDragOver={handleDragOver}>
-                    <span>Drag files here</span>
-                </div>
-                    <input type="file" id="file-input" accept="image/*" onChange={handleFileInputChange} multiple />
-                <div className="browse-container">
-                    <div className="browse-item" onClick={handleBrowseClick}>
-                        <span>or click to browse</span>
-                    </div>
-                </div>
-                <label> Description:
-                    <textarea maxlength="120"
-                        name="description"
-                        defaultValue={inputs.description}
-                        onChange={handleChange}
-                    />
-                </label>
-                
-                <label>Amount:
-                    <input
-                        type="number"
-                        name="amount"
-                        defaultValue={inputs.amount}
-                        onChange={handleChange}
-                    />
-                </label>
-                
-                <label>Location:
-                    <input
-                        type="text"
-                        name="location"
-                        defaultValue={inputs.location}
-                        onChange={handleChange}
-                    />
-                </label>
-                
-                <label>Number of bedrooms:
-                    <select name="bed" 
-                        value={inputs.bed || ""} 
-                        onChange={handleChange}>
-                            {Array.from(Array(10).keys()).map((value) => (
-                            <option key={value} value={value + 1}>{value + 1}</option>
-                            ))}
-                    </select>
-                </label>
+        <section id="main-container">
+            <NavBar />
+            <section id="main-form-container">
+                <section id="form-container">
+                    <form id="new-property-form">
+                        {error && <p className="error">{error}</p>}   
+                        <div className="input-field">
+                            <label> Description:
+                                <textarea maxlength="120"
+                                    name="description"
+                                    defaultValue={inputs.description}
+                                    onChange={handleChange}
+                                />
+                            </label>
+                        </div>
 
+                        <div className="input-field">
+                            <label>Amount:
+                                <input
+                                    type="number"
+                                    name="amount"
+                                    defaultValue={inputs.amount}
+                                    onChange={handleChange}
+                                />
+                            </label>
+                        </div>
 
-                <label>Number of bathroom:
-                    <select name="bath" 
-                        value={inputs.bath || ""} 
-                        onChange={handleChange}>
-                            {Array.from(Array(10).keys()).map((value) => (
-                            <option key={value} value={value + 1}>{value + 1}</option>
-                            ))}
-                    </select>
-                </label>
+                        <div className="input-field">                        
+                            <label>Location:
+                                <input
+                                    type="text"
+                                    name="location"
+                                    defaultValue={inputs.location}
+                                    onChange={handleChange}
+                                />
+                            </label>
+                        </div>
 
-                <label>Number of toilet:
-                    <select name="toilet" 
-                        value={inputs.toilet || ""} 
-                        onChange={handleChange}>
-                            {Array.from(Array(10).keys()).map((value) => (
-                            <option key={value} value={value + 1}>{value + 1}</option>
-                            ))}
-                    </select>                    
-                </label>
+                        <section id="form-group">
+                            <div className="input</div>-field">                        
+                                <label>Number of bedrooms:
+                                    <select name="bed" 
+                                        value={inputs.bed || ""} 
+                                        onChange={handleChange}>
+                                            {Array.from(Array(10).keys()).map((value) => (
+                                            <option key={value} value={value + 1}>{value + 1}</option>
+                                            ))}
+                                    </select>
+                                </label>
+                            </div>
 
-                <label>Property for:
-                    <select name="action" onChange={handleChange}>
-                        <option value="">Choose option</option>
-                        <option value="Rent">Rent</option>
-                        <option value="Sale">Sale</option>
-                        <option value="Rent/Sale">Rent/Sale</option>
-                    </select>
-                </label>
+                                
+                                    <label>Number of bathroom:
+                                        <select name="bath" 
+                                            value={inputs.bath || ""} 
+                                            onChange={handleChange}>
+                                                {Array.from(Array(10).keys()).map((value) => (
+                                                <option key={value} value={value + 1}>{value + 1}</option>
+                                                ))}
+                                        </select>
+                                    </label>
 
-                <label>Status of property:
-                    <input 
-                        type="radio" 
-                        id="status-new"
-                        name="status" 
-                        value="New"
-                    />
-                    <label for="status-new">New</label>
-                    <input 
-                        type="radio" 
-                        id="status-renovated"
-                        name="status" 
-                        value="Renovated"
-                    />
-                    <label for="status-renovated">Renovated</label>                    
-                </label>
+                                    <label>Number of toilet:
+                                        <select name="toilet" 
+                                            value={inputs.toilet || ""} 
+                                            onChange={handleChange}>
+                                                {Array.from(Array(10).keys()).map((value) => (
+                                                <option key={value} value={value + 1}>{value + 1}</option>
+                                                ))}
+                                        </select>                    
+                                    </label>
+                        </section>
 
-            </form>
-        </>
+                            <label>Property for:
+                                <select name="action" onChange={handleChange}>
+                                    <option value="">Choose option</option>
+                                    <option value="Rent">Rent</option>
+                                    <option value="Sale">Sale</option>
+                                    <option value="Rent/Sale">Rent/Sale</option>
+                                </select>
+                            </label>
+
+                        <label>Status of property:
+                            <input 
+                                type="radio" 
+                                id="status-new"
+                                name="status" 
+                                value="New"
+                            />
+                            <label for="status-new">New</label>
+                            <input 
+                                type="radio" 
+                                id="status-renovated"
+                                name="status" 
+                                value="Renovated"
+                            />
+                            <label for="status-renovated">Renovated</label>                    
+                        </label>
+
+                        <div className="draggable-item" draggable={true} onDragStart={handleDragStart} onDragOver={handleDragOver}>
+                            <span>Drag files here</span>
+                        </div>
+                            <input type="file" id="file-input" accept="image/*" onChange={handleFileInputChange} multiple />
+                        <div className="browse-container">
+                            <div className="browse-item" onClick={handleBrowseClick}>
+                                <span>or click to browse</span>
+                            </div>
+                        </div>
+
+                        <input type="submit" value="Submit" className="action" />
+
+                    </form>
+                </section>
+            </section>
+        </section>
     )
 }
 
